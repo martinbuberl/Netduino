54b60036120beb3f657645257e489b9d8c55606f
martinbuberl <mail@martinbuberl.com>
2013-01-17 17:17:16 -0500

    Add breadboard LED

--- src/Blinky/Program.cs
+++ src/Blinky/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
 using System.Threading;
-using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
-using SecretLabs.NETMF.Hardware;
 using SecretLabs.NETMF.Hardware.Netduino;
 
 namespace Blinky
@@ -12,6 +8,7 @@ namespace Blinky
     public class Program
     {
         private static readonly OutputPort OnboardLed = new OutputPort(Pins.ONBOARD_LED, false);
+        private static readonly OutputPort BreadboardLed = new OutputPort(Pins.GPIO_PIN_D0, false);
 
         public static void Main()
         {
@@ -24,19 +21,30 @@ namespace Blinky
                 Thread.Sleep(100);
             }
 
-            // button on/off on push
-            InterruptPort button = new InterruptPort(Pins.ONBOARD_SW1, false, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeHigh);
-            button.OnInterrupt += button_OnInterrupt;
+            // onboard button pushed turns onboard led on/off
+            InterruptPort buttonOnboard = new InterruptPort(Pins.ONBOARD_SW1, false, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeHigh);
+            buttonOnboard.OnInterrupt += buttonOnboard_OnInterrupt;
+
+            // breadboard button pushed turns breadboard led on/off
+            InterruptPort buttonBreadboard = new InterruptPort(Pins.GPIO_PIN_D1, false, Port.ResistorMode.PullUp, Port.InterruptMode.InterruptEdgeHigh);
+            buttonBreadboard.OnInterrupt += buttonBreadboard_OnInterrupt;
 
             Thread.Sleep(Timeout.Infinite);
         }
 
         private static bool _onboardLedState;
+        private static bool _breadboardLedState;
 
-        static void button_OnInterrupt(uint data1, uint data2, DateTime time)
+        static void buttonOnboard_OnInterrupt(uint data1, uint data2, DateTime time)
         {
             _onboardLedState = !_onboardLedState;
             OnboardLed.Write(_onboardLedState);
+        }
+
+        static void buttonBreadboard_OnInterrupt(uint data1, uint data2, DateTime time)
+        {
+            _breadboardLedState = !_breadboardLedState;
+            BreadboardLed.Write(_breadboardLedState);
         }
     }
 }
