6c1cae225df4ed22009182c40eff44ac7486a9ed
martinbuberl <mail@martinbuberl.com>
2013-01-17 17:19:50 -0500

    Add new project to control potentiometer

---
+++ src/Potentiometer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+namespace Potentiometer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            AnalogInput pot = new AnalogInput(Cpu.AnalogChannel.ANALOG_0);
+
+            while (true)
+            {
+                Debug.Print(pot.Read().ToString());
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
